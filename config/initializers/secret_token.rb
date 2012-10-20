0a9b2383050f28b47129a44afd0172f22616a64c
willy <waitingkuo0527@gmail.com>
2012-10-20 15:21:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mayday::Application.config.secret_token = 'bc1642108319ef09af3f1ae9d6129efe23824b9332e758f198fb26d5aff8cfd9fdb1597be3b0f5378d5515762cc45f19d4f17dfc2b7da17fe6396ffff5b9bd02'
